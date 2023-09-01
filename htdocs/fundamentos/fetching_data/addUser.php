de36ee4b41a4fda45c90443a8ca6d34e7f7f48df
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-01 20:13:40 -0300

    chore: abstracts log errors config to separated module

--- htdocs/fundamentos/fetching_data/addUser.php
+++ htdocs/fundamentos/fetching_data/addUser.php
@@ -1,10 +1,13 @@
 <?php
 
+    require 'displayErrorsConfig.php';
     require 'dbconfig.php';
 
     $eMail = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
     $password = filter_input(INPUT_POST,'password');
 
     $pdo->query("INSERT INTO usuarios (email, senha) VALUES ('$eMail', '$password')");
+
+    
 
     header("location: index.php");
--- htdocs/fundamentos/fetching_data/index.php
+++ htdocs/fundamentos/fetching_data/index.php
@@ -1,9 +1,7 @@
 <?php
 
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
-
+    
+    require 'displayErrorsConfig.php';
     require 'dbconfig.php';
     
     $query = $pdo->query('SELECT * FROM usuarios');
