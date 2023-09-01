ec5a875aa0bd9a54bebeaf7501a2eeec934010d1
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-01 18:08:04 -0300

    feat: implements data-fetchin code using PDO class - fetchin_data project

---
+++ htdocs/fundamentos/fetching_data/index.php
@@ -0,0 +1,15 @@
+<?php
+
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+
+    $pdo = new PDO('mysql:dbname=test;host=localhost', 'root', '');
+    $query = $pdo->query('SELECT * FROM usuarios');
+    $data = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo '<pre>';
+    print_r($data);
+    print_r($data[1]["senha"]); 
+    
+
+?>
