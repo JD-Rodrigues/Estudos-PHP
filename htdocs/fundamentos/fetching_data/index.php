d7353dec72f33db1bf81a7fee4ad4eb6cd0633ba
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-02 11:18:17 -0300

    feat: renders users data in a table - fetch_data project

--- htdocs/fundamentos/fetching_data/index.php
+++ htdocs/fundamentos/fetching_data/index.php
@@ -2,11 +2,23 @@
     
     require 'displayErrorsConfig.php';
     require 'getUsers.php';
-    
-    $users = $preparedUsers->fetchAll(PDO::FETCH_ASSOC);
-    
-    echo '<pre>';
 
-    print_r($users)
+    $users = $preparedUsers->fetchAll(PDO::FETCH_ASSOC);  
     
 ?>
+<h1 style="text-align: center">Usuários</h1>
+<table border='1' width='50%' style="margin: auto;">
+    <tr>
+        <th>EMAIL</th>
+        <th>SENHA</th>
+    </tr>
+
+    <?php foreach($users as $user): ?>
+        <tr>
+            <td><?=$user['email']?></td>
+            <td><?=$user['senha']?></td>
+            <td><a href="editUser.php?id=<?=$user['id']?>">Editar</a></td>
+            <td><a href="delUser.php?id=<?=$user['id']?>">Remover</a></td>
+        </tr>
+    <?php endforeach; ?>
+</table>
