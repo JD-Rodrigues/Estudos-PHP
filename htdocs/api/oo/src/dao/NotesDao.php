5ee8dc6be842ad780ff1122b9c99931cb725cba0
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-06 15:55:33 -0300

    feat: implements NotesDao->createNote() - api/oo project

--- htdocs/api/oo/src/dao/NotesDao.php
+++ htdocs/api/oo/src/dao/NotesDao.php
@@ -21,7 +21,10 @@ class NotesDao implements NotesDaoInterface {
     }
 
     public function createNote(Note $noteObject){
-
+        $preparedNotes = $this->pdo->prepare("INSERT INTO notes (title, body) VALUES (:title, :body)");
+        $preparedNotes->bindValue(':title', $noteObject->title);
+        $preparedNotes->bindValue(':body', $noteObject->body);
+        $preparedNotes->execute();
     }
 
     public function updateNote(int $id){
