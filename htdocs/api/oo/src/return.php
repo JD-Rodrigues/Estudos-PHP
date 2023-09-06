4975d6f710842b218ba8ddcccebd876e9247e101
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-06 13:21:17 -0300

    chore: configures return file to serve endpoints out - api/oo project

---
+++ htdocs/api/oo/src/return.php
@@ -0,0 +1,8 @@
+<?php
+require 'apiConfig.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Content-Type: application/json");
+echo json_encode($apiData);
+exit;
