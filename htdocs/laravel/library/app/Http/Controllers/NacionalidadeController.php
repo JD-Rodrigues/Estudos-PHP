6000ed34f9e7e5d8e33564134341ea40fe9a3ba0
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-09 17:12:50 -0300

    feat: implements index() method in NacionalidadeController - Laravel/library project

--- htdocs/laravel/library/app/Http/Controllers/NacionalidadeController.php
+++ htdocs/laravel/library/app/Http/Controllers/NacionalidadeController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Nacionalidade;
 
 class NacionalidadeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Nacionalidade::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        return Nacionalidade::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Example $example)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Example $example)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Example $example)
+    {
+        //
+    }
 }
