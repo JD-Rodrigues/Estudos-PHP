295755b63537c72bd97d493e8507ba8fcaa04fd9
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-09 18:22:24 -0300

    feat: implements update() method in NacionalidadeController - Laravel/library project

--- htdocs/laravel/library/app/Http/Controllers/NacionalidadeController.php
+++ htdocs/laravel/library/app/Http/Controllers/NacionalidadeController.php
@@ -36,9 +36,26 @@ class NacionalidadeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Example $example)
+    public function update(Request $request)
     {
-        //
+        $nacionalidadeExists = Nacionalidade::find($request->input('id'));
+
+        if ($nacionalidadeExists) {
+
+            $requestsArray = $request->all();
+
+            foreach($requestsArray as $input=>$value) {
+                if(array_key_exists($input, $requestsArray)) {
+                    $nacionalidadeExists->$input = $value;
+                }
+            }
+            $nacionalidadeExists->save();
+            return $nacionalidadeExists;
+        } else {
+            return ["Erro:"=>"Não há nacionalidade com o id informado no banco de dados."];
+        }
+
+        
     }
 
     /**
