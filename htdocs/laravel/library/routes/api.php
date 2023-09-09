2c02e086277022d9ec17b13e966148e9d03713a7
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-08 21:36:08 -0300

    feat: implements all the Authors routes

--- htdocs/laravel/library/routes/api.php
+++ htdocs/laravel/library/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivroController;
+use App\Http\Controllers\AuthorController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +16,16 @@ use App\Http\Controllers\LivroController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::post('/livros', [LivroController::class, 'store']);
+Route::post('/livro', [LivroController::class, 'store']);
 Route::get('/livros', [LivroController::class, 'index']);
 Route::get('/livro', [LivroController::class, 'show']);
 Route::put('/livro', [LivroController::class, 'update']);
 Route::delete('/livro', [LivroController::class, 'destroy']);
+Route::post('/autor', [AuthorController::class, 'store']);
+Route::get('/autores', [AuthorController::class, 'index']);
+Route::get('/autor', [AuthorController::class, 'show']);
+Route::put('/autor', [AuthorController::class, 'update']);
+Route::delete('/autor', [AuthorController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
