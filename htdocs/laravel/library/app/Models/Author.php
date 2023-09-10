df294b35fc429b50a105055993e4290b45a633d1
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-10 10:23:10 -0300

    chore: configures soft deleting in Author model

--- htdocs/laravel/library/app/Models/Author.php
+++ htdocs/laravel/library/app/Models/Author.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'ano_de_nascimento', 'nacionalidade_id'];
+    use SoftDeletes;
 }
