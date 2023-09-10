f29c5439541c2d76e13db243dd73fa7f8cd08654
JD_Rodrigues <jdevproject01@gmail.com>
2023-09-10 10:24:08 -0300

    feat: implements customized validation messages in store method os AuthorController

--- htdocs/laravel/library/app/Http/Controllers/AuthorController.php
+++ htdocs/laravel/library/app/Http/Controllers/AuthorController.php
@@ -5,11 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Author;
 
+
 class AuthorController extends Controller
 {
     public function index()
     {
-        return Author::all();
+        try {
+            return Author::all();
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
 
     /**
@@ -17,7 +22,30 @@ class AuthorController extends Controller
      */
     public function store(Request $request)
     {
-        return Author::create($request->all());
+        $rules = [
+            'nome'=>'string|required|max:255',
+            'ano_de_nascimento'=>'required|integer|max_digits:4',
+            'nacionalidade_id'=>'integer|required|max:20'
+        ];
+
+        $messages = [
+            'nome.string'=>'O nome precisa ser valor do tipo texto.',
+            'nome.required'=>'Você precisa enviar um nome.',
+            'ano_de_nascimento.required'=>'Você precisa informar o ano de nascimento.',
+            'ano_de_nascimento.integer'=>'O ano de nascimento tem de ser um número.',
+            'ano_de_nascimento.max_digits'=>'O ano de nascimento não pode exceder 4 dígitos.'
+        ];
+        
+        try {
+            $request->validate($rules, $messages);
+
+            return Author::create($request->all());
+
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+        
+        
     }
 
     /**
@@ -25,9 +53,20 @@ class AuthorController extends Controller
      */
     public function show(Request $request)
     {
-        $id = $request->input('id');
+        try {
+            $request->validate([
+                'id'=>'required|integer',
+            ]);
 
-        return Author::find($id);
+            $id = $request->input('id');
+
+            return Author::findOrFail($id);
+
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+        
+        
     }
 
     /**
@@ -35,17 +74,31 @@ class AuthorController extends Controller
      */
     public function update(Request $request)
     {
-        $authorExists = Author::findOrFail($request->input('id'));
+        try {
+            $request->validate([
+                'id'=>'integer|required',
+                'nome'=>'string|max:255',
+                'ano_de_nascimento'=>'integer|max_digits:4',
+                'nacionalidade_id'=>'integer|max:20'
+            ]);
 
-        $requestArray = $request->all();
+            $authorExists = Author::findOrFail($request->input('id'));
 
-        foreach($requestArray as $input=>$value) {
-            if(array_key_exists($input, $authorExists->getAttributes())) {
-                $authorExists->$input = $value;
+            $requestArray = $request->all();
+
+            foreach($requestArray as $input=>$value) {
+                if(array_key_exists($input, $authorExists->getAttributes())) {
+                    $authorExists->$input = $value;
+                }
             }
-        }
 
-        $authorExists->save();
+            $authorExists->save();
+            return $authorExists;
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+        
+        
 
     }
 
@@ -54,8 +107,17 @@ class AuthorController extends Controller
      */
     public function destroy(Request $request)
     {
-        $authorExists = Author::find($request->input('id'));
-
-        $authorExists && $authorExists->delete();
+        try {
+            $request->validate([
+                'id'=>'required|integer'
+            ]);
+            
+            $authorExists = Author::findOrFail($request->input('id'));
+    
+            $authorExists->delete();
+            return "O autor de id $authorExists->id foi removido.";
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
     }
 }
